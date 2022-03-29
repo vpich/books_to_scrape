e215d2e67532f303503af41fd2b645c30f0e3be2
Virginie PICH <red_iv@hotmail.fr>
2022-03-29 21:55:36 +0200

    Suppression de la fonction check_number_pages() + dans la fonction get_all_books_urls: réarrangement de la boucle while pour la pagination, et correction de la boucle "for book in all_books" pour aller chercher chaque url des livres, sauf s'il n'y en a qu'un seul dans la catégorie (pour éviter que la boucle se lance 2 fois)

--- get_books_url.py
+++ get_books_url.py
@@ -5,44 +5,34 @@ from get_categories_url import get_categories
 home_page_url = "http://books.toscrape.com/"
 
 
-def check_number_pages(url):
-    page_to_check = requests.get(url)
-    soup = BeautifulSoup(page_to_check.content, "html.parser")
-
-    next_button = soup.find_all("div", class_="next")
-    print(next_button)
-
-    if page_to_check.status_code == 200:
-        pass
-
-
-def get_all_books(category):
+def get_all_books_urls(category):
 
     books_url = []
     base_category_url = home_page_url + category
 
-    for i in range(20):
-    # i = 0
-    # while True:
-
+    i = 0
+    while True:
         category_url = base_category_url
-        if i > 0:
-            category_url = base_category_url.replace("index.html", "page-" + i + ".html")
+        if i > 1:
+            category_url = base_category_url.replace("index.html", "page-" + str(i) + ".html")
         page = requests.get(category_url)
         if i == 1:
             pass
         if page.status_code != 200:
             break
-        soup = BeautifulSoup(page.content, "html.parser")
 
+        soup = BeautifulSoup(page.content, "html.parser")
         all_books = soup.select("h3 > a")
 
         for book in all_books:
-            book_url = str(book.get("href")).strip("../../..")
-            books_url.append(home_page_url + "catalogue/" + book_url)
-        # i += 1
+            base_book_url = str(book.get("href")).strip("../../..")
+            book_url = home_page_url + "catalogue/" + base_book_url
+            if book_url in books_url:
+                break
+            books_url.append(book_url)
+        i += 1
     return books_url
 
 
 if __name__ == "__main__":
-    print(get_all_books(get_categories(home_page_url)[0]))
+    print(get_all_books_urls(get_categories(home_page_url)[0]))
