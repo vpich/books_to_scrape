8062cf2f721aa1b4b872cecab5d1c5ff192527b7
Virginie PICH <red_iv@hotmail.fr>
2022-03-29 21:56:16 +0200

    Suppression de la fonction check_number_pages()

--- get_categories_url.py
+++ get_categories_url.py
@@ -16,17 +16,6 @@ def get_categories(url):
 
     return categories_url
 
-# def check_number_pages(url, page):
-#     page_to_check_url = home_page_url + str(url).replace("index.html", page)
-#     page_to_check = requests.get(page_to_check_url)
-#     if page_to_check.status_code == 200:
-#         return page_to_check_url
-#     else:
-#         return url
-
-
-
 
 if __name__ == "__main__":
-    #print(get_categories("http://books.toscrape.com/"))
-    check_number_pages("https://books.toscrape.com/catalogue/category/books/mystery_3/index.html")
+    print(get_categories("http://books.toscrape.com/"))
