646110f535d3c26d1ede5a3ae9e2c3e396c24202
Virginie PICH <red_iv@hotmail.fr>
2022-03-30 18:42:02 +0200

    Remplacement d'une portion de code par l'import de la fonction save_data_to_csv() + première tentative pour séparer dans différents fichiers csv

--- main.py
+++ main.py
@@ -1,12 +1,10 @@
-import csv
-
 from get_images import get_images
+from save_data_to_csv import save_data_to_csv
 from variables import get_book_datas
 from get_categories_url import get_categories
 from get_books_url import get_all_books_urls
 
 home_page_url = "http://books.toscrape.com/"
-
 
 if __name__ == "__main__":
 
@@ -18,33 +16,21 @@ if __name__ == "__main__":
         for book in books_in_category:
             all_books_url.append(book)
 
-    header_titles = [
-        "product_page_url",
-        "universal_product_code",
-        "title",
-        "price_including_tax",
-        "price_excluding_tax",
-        "number_available",
-        "product_description",
-        "category",
-        "review_rating",
-        "image_url"
-    ]
-
     all_books_list = []
 
     for book_url in all_books_url:
         book_data = get_book_datas(book_url)
         all_books_list.append(book_data)
+        book_category = book_data[7]
 
-        get_images(book_data[9], book_data[1])
+        get_images(book_data[9], book_data[1], book_category)
 
-    with open("data.csv", "w", encoding="utf-8") as f:
-        writer = csv.writer(f, delimiter=",")
-        writer.writerow(header_titles)
-        for book in all_books_list:
-            writer.writerow(book)
+    save_data_to_csv("data.csv", all_books_list)
 
+    # Le code ci-dessous devrait permettre de séparer chaque catégorie dans des fichiers csv disctincts, mais ne fonctionne pas
+    books_per_cat = {}
+    for book in all_books_list:
+        books_per_cat[book[7]].append(book)
 
-
-# oubli: chaque categorie doit être un fichier dans un fichier csv différent
+    for category in books_per_cat:
+        save_data_to_csv(category, books_per_cat[category])
