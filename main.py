d8bfe5fbcdc548621d9cc372b3c9b3a724cc8779
Virginie PICH <red_iv@hotmail.fr>
2022-03-31 17:11:37 +0200

    Renommage des variables pour plus de lisibilité.

--- main.py
+++ main.py
@@ -1,39 +1,43 @@
-from get_images import get_images
+from get_images import save_images
 from save_data_to_csv import save_data_to_csv
-from variables import get_book_datas
-from get_categories_url import get_categories
+from variables import get_book_data
+from get_categories_url import get_categories_urls
 from get_books_url import get_all_books_urls
 
-home_page_url = "http://books.toscrape.com/"
+HOME_PAGE_URL = "http://books.toscrape.com/"
 
 if __name__ == "__main__":
 
-    categories_url_list = get_categories(home_page_url)
-    all_books_url = []
+    all_categories_urls = get_categories_urls(HOME_PAGE_URL)
+    all_books_urls = []
 
-    for category in categories_url_list:
-        books_in_category = get_all_books_urls(category)
-        for book in books_in_category:
-            all_books_url.append(book)
+    for category_url in all_categories_urls:
+        books_urls_in_category = get_all_books_urls(category_url)
+        for book_url_in_category in books_urls_in_category:
+            all_books_urls.append(book_url_in_category)
 
     all_books_list = []
     all_categories_list = []
 
-    for book_url in all_books_url:
-        book_data = get_book_datas(book_url)
+    for book_url in all_books_urls:
+        book_data = get_book_data(book_url)
         all_books_list.append(book_data)
+
         book_category = book_data[7]
+        book_image_url = book_data[9]
+        book_upc = book_data[1]
 
         if book_category not in all_categories_list:
             all_categories_list.append(book_category)
 
-        get_images(book_data[9], book_data[1], book_category)
+        save_images(book_image_url, book_upc, book_category)
 
     i = 0
     while i < len(all_categories_list):
-        books_in_cat = []
+        books_in_category = []
         for book_data in all_books_list:
-            if book_data[7] == all_categories_list[i]:
-                books_in_cat.append(book_data)
-                save_data_to_csv(book_data[7], books_in_cat)
+            book_category = book_data[7]
+            if book_category == all_categories_list[i]:
+                books_in_category.append(book_data)
+                save_data_to_csv(book_category, books_in_category)
         i += 1
