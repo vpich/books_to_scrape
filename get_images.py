b57cde192f7a9e367e16e000c2d834f8e3291f32
Virginie PICH <red_iv@hotmail.fr>
2022-03-31 17:28:47 +0200

    Changement du nom de la fonction de get_images en save_images

--- get_images.py
+++ get_images.py
@@ -2,7 +2,7 @@ import requests
 from pathlib import Path
 
 
-def get_images(image_url, upc, directory_name):
+def save_images(image_url, upc, directory_name):
     image = requests.get(image_url).content
     image_type = Path(image_url).suffix
     image_directory = Path(f"images/{directory_name}")
@@ -15,5 +15,5 @@ def get_images(image_url, upc, directory_name):
 
 
 if __name__ == "__main__":
-    get_images("https://books.toscrape.com/media/cache/9f/25/9f25ffe4229a32d1368b3dfe248c3343.jpg", "a897fe39b1053632", "test")
+    save_images("https://books.toscrape.com/media/cache/9f/25/9f25ffe4229a32d1368b3dfe248c3343.jpg", "a897fe39b1053632", "test")
 
