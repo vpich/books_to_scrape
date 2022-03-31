9bcea8d6cd3887d3846064f9bd371747019e26f3
Virginie PICH <red_iv@hotmail.fr>
2022-03-31 17:24:01 +0200

    Renommage des variables et fonction pour plus de lisibilité.

--- get_categories_url.py
+++ get_categories_url.py
@@ -2,20 +2,20 @@ from bs4 import BeautifulSoup
 import requests
 
 
-def get_categories(url):
+def get_categories_urls(home_page_url):
 
-    home_page = requests.get(url)
+    home_page = requests.get(home_page_url)
     soup = BeautifulSoup(home_page.content, "html.parser")
 
     all_categories = soup.select("ul.nav-list > li > ul > li > a")
 
-    categories_url = []
+    categories_urls = []
 
-    for cat in all_categories:
-        categories_url.append(cat.get("href"))
+    for category in all_categories:
+        categories_urls.append(category.get("href"))
 
-    return categories_url
+    return categories_urls
 
 
 if __name__ == "__main__":
-    print(get_categories("http://books.toscrape.com/"))
+    print(get_categories_urls("http://books.toscrape.com/"))
