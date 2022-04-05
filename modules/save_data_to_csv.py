b5ff95c4c8a01285a02673e1625b5fb6e5e82e42
Virginie PICH <red_iv@hotmail.fr>
2022-04-05 15:17:40 +0200

    Transformation de la liste header_titles en constante HEADER_TITLES pour qu'elle soit en dehors de la fonction

--- modules/save_data_to_csv.py
+++ modules/save_data_to_csv.py
@@ -1,21 +1,21 @@
 import csv
 from pathlib import Path
 
+HEADER_TITLES = [
+    "product_page_url",
+    "universal_product_code",
+    "title",
+    "price_including_tax",
+    "price_excluding_tax",
+    "number_available",
+    "product_description",
+    "category",
+    "review_rating",
+    "image_url"
+]
+
 
 def save_data_to_csv(csv_name, books_list):
-
-    header_titles = [
-        "product_page_url",
-        "universal_product_code",
-        "title",
-        "price_including_tax",
-        "price_excluding_tax",
-        "number_available",
-        "product_description",
-        "category",
-        "review_rating",
-        "image_url"
-    ]
 
     csv_directory = Path("csv")
     if not csv_directory.exists():
@@ -23,6 +23,6 @@ def save_data_to_csv(csv_name, books_list):
 
     with open(f"{csv_directory}/{csv_name}.csv", "w", encoding="utf-8") as f:
         writer = csv.writer(f, delimiter=",")
-        writer.writerow(header_titles)
+        writer.writerow(HEADER_TITLES)
         for book in books_list:
             writer.writerow(book)
